42ca4c015e84b534d88f730f60dabfc38879d25b
Matt Fasano <matt.fasano88@gmail.com>
2019-05-09 00:55:07 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -54,6 +54,14 @@ server.express.use(async (req, res, next) => {
   next();
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 server.start(
   {
     cors: {
